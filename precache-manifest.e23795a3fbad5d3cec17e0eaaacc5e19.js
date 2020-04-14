4d91da46419ac16c875373f017e97a5fd457095d
DangThanhTrung1999 <pumaover@gmail.com>
2020-04-14 14:50:53 +0700

    Updates

---
+++ precache-manifest.e23795a3fbad5d3cec17e0eaaacc5e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9148a5598b2c174d2604249d66bf489f",
+    "url": "/NoteFireBase/index.html"
+  },
+  {
+    "revision": "17db8996eaf9dbb08d73",
+    "url": "/NoteFireBase/static/css/main.aa4d434c.chunk.css"
+  },
+  {
+    "revision": "09ed5d8424b3654aa31a",
+    "url": "/NoteFireBase/static/js/2.ca79c54a.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/NoteFireBase/static/js/2.ca79c54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17db8996eaf9dbb08d73",
+    "url": "/NoteFireBase/static/js/main.d66c3762.chunk.js"
+  },
+  {
+    "revision": "223a308fcde06fc7194b",
+    "url": "/NoteFireBase/static/js/runtime-main.74a10155.js"
+  }
+]);
